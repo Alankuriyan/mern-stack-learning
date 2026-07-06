698a05d1f120da57783fc78aeaf38b43e9aa4a3d
alan <akjyofficial@gmail.com>
2026-07-06 23:38:00 +0530

    note app added upto storage

--- easy/note app/script.js
+++ easy/note app/script.js
@@ -1,3 +1,30 @@
+
+
 function addnote(){
- const container = document.getElementById("container").innerHTML="hello"
+ const note = document.createElement('div')
+ note.className = " note"
+ note.textContent = "note"
+ const conatainer = document.getElementById("note-container")
+ conatainer.append(note)
+ const textarea =  document.createElement('input')
+ textarea.id = "inputfeild"
+ textarea.placeholder = "write here"
+ note.appendChild(textarea)
+ const save = document.createElement('button')
+ save.textContent="save"
+ save.onclick = savefunction;
+ note.appendChild(save)
+ function savefunction(){
+   
+   
+    const obj ={
+        id :  Date.now(),
+        message : textarea.value
+    }
+
+   const arr = JSON.parse(localStorage.getItem("mess")) ||[]
+    arr.push(obj);
+   localStorage.setItem("mess", JSON.stringify(arr))
+}
+ 
 }
