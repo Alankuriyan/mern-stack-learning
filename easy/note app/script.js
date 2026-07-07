12cbe6d70f8eda9c4aecefecb0a579ea5ea7a317
alan <akjyofficial@gmail.com>
2026-07-07 19:27:54 +0530

    note app added

--- easy/note app/script.js
+++ easy/note app/script.js
@@ -3,7 +3,7 @@
 function addnote(){
  const note = document.createElement('div')
  note.className = " note"
- note.textContent = "note"
+ note.textContent = "";
  const conatainer = document.getElementById("note-container")
  conatainer.append(note)
  const textarea =  document.createElement('input')
@@ -25,6 +25,42 @@ function addnote(){
    const arr = JSON.parse(localStorage.getItem("mess")) ||[]
     arr.push(obj);
    localStorage.setItem("mess", JSON.stringify(arr))
+
+   let output = ""
+   for(let x of arr)
+   {
+        console.log(x.message,x.id)
+           output += x.id + x.message + "<br>";
+           note.className = "note";
+
+    note.dataset.id = x.id;
+   }
+    
+   document.getElementById('display').innerHTML = output
+       
+       
 }
  
+
 }
+function loadnote (){
+   
+    const conatainer = document.getElementById("note-container");
+    const arra = JSON.parse(localStorage.getItem("mess")) || [];
+    for(let x of arra){
+       const oldnote =  document.createElement("div")
+       oldnote.className ="note"
+       oldnote.dataset.id = x.id
+        const inp = document.createElement("input")
+        inp.value = x.message
+        const save2 = document.createElement('button')
+ save2.textContent="save"
+
+ 
+        conatainer.append(oldnote)
+    oldnote.appendChild(inp)
+    oldnote.appendChild(save2)
+    }
+    
+}
+loadnote()
